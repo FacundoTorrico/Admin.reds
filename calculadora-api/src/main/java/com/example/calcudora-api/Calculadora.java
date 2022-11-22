2afe3102f8e33f2792d5eafaa5ccdf189b2c639d
FacundoTorrico <86027492+FacundoTorrico@users.noreply.github.com>
2022-11-22 17:45:36 -0300

    fix

--- calculadora-api/src/main/java/com/example/calcudora-api/Calculadora.java
+++ calculadora-api/src/main/java/com/example/calcudora-api/Calculadora.java
@@ -2,10 +2,17 @@ package com.example.calculadora-api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 public class Calculadora 
 {
 
+        @GetMapping("/hola")
+        String ruta()
+	{
+        return "Hola mundo";
+    	}
+	
 	@GetMapping("/suma/{num1}/{num2}")
 	public static int suma(@PathVariable int num1, @PathVariable int num2) 
 	{
@@ -33,4 +40,26 @@ public class Calculadora
 		}
 		return num1 / num2;
 	}
+	
+	public static float[] FuncionCuadratica(float a, float b, float c)
+	{       
+        float b2 = b*b;
+        float dentroraiz = b2 - (4*a*c);
+        float raiz = (float) Math.sqrt(dentroraiz);
+        float cuenta = (float)raiz;
+        float verificacion =cuenta;
+            if (verificacion % 2 ==0)
+            {
+                float primero = ((-b + raiz)/2*a);
+                float segundo = ((-b  - raiz)/2*a);
+
+                float [] result = {primero, segundo};
+
+                return  result;
+            }
+     
+	 return null;
+
+         }
+	
 }
